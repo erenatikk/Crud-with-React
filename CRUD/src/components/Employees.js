249f1c067aa2bb296e0bb78975894a96b5eb9431
erenatikk <erenatik7@outlook.com>
2022-11-19 13:59:49 +0300

    first commit

---
+++ CRUD/src/components/Employees.js
@@ -0,0 +1,20 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Eren Atik",
+        Age: "21"
+    },
+    {
+        id:"2",
+        Name:"Recep Güneş",
+        Age: "22"
+    },
+    {
+        id:"3",
+        Name:"Ahmet Eren Güner",
+        Age: "23"
+    }
+]
+
+ 
+export default  Employees;
